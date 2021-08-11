1513bab95cc648de6071e9a125c621fbcb9b8c1c
kidculli <9029776+kidculli@users.noreply.github.com>
2021-08-11 14:57:11 -0400

    chore: init

---
+++ index.js
@@ -0,0 +1,15 @@
+const tinyurl = require("tinyurl");
+const testJSON = JSON.stringify(require("./test.json"));
+const lzma = require("json-url")("lzw");
+
+async function test() {
+  const compressed = await lzma.compress(testJSON);
+  console.log(compressed.length);
+  const result = await tinyurl.shorten(
+    `http://localhost:8000?q={${compressed}}`
+  );
+  //console.log(compressed);
+  console.log(result);
+}
+
+test();
