e90a33fb8d0a180da7fb42384195d1f93af48a15
kidculli <9029776+kidculli@users.noreply.github.com>
2021-08-11 15:30:20 -0400

    test: catch rejected, decrease interval

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const lzma = require("json-url")("lzma");
 async function test() {
   const compressed = await lzma.compress(testJSON);
   console.log(compressed.length);
-  const result = await tinyurl.shorten(
-    `http://localhost:8000?t=${new Date().getTime()}&q=${compressed}`
-  );
+  const result = await tinyurl
+    .shorten(`http://localhost:8000?t=${new Date().getTime()}&q=${compressed}`)
+    .catch(console.log);
   //console.log(compressed);
   console.log(result);
   //const decompressed = await lzma.decompress(compressed);
   //console.log(decompressed);
 }
 
-setInterval(test, 500);
+setInterval(test, 50);
