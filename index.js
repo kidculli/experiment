833e4c8cbdffdbdfa8ce9c9dc7b04f1ec7ed48f7
kidculli <9029776+kidculli@users.noreply.github.com>
2021-08-11 15:18:25 -0400

    test:randomize url and call setinterval

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const lzma = require("json-url")("lzma");
 async function test() {
   const compressed = await lzma.compress(testJSON);
   console.log(compressed.length);
-  const result = await tinyurl.shorten(`http://localhost:8000?q=${compressed}`);
+  const result = await tinyurl.shorten(
+    `http://localhost:8000?t=${new Date().getTime()}&q=${compressed}`
+  );
   //console.log(compressed);
   console.log(result);
+  //const decompressed = await lzma.decompress(compressed);
+  //console.log(decompressed);
 }
 
-test();
+setInterval(test, 500);
