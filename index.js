f4173d08167534a8175cbd7ee24a74567e062901
kidculli <9029776+kidculli@users.noreply.github.com>
2021-08-11 15:07:08 -0400

    perf: use lzma

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const tinyurl = require("tinyurl");
 const testJSON = JSON.stringify(require("./test.json"));
-const lzma = require("json-url")("lzw");
+const lzma = require("json-url")("lzma");
 
 async function test() {
   const compressed = await lzma.compress(testJSON);
   console.log(compressed.length);
-  const result = await tinyurl.shorten(
-    `http://localhost:8000?q={${compressed}}`
-  );
+  const result = await tinyurl.shorten(`http://localhost:8000?q=${compressed}`);
   //console.log(compressed);
   console.log(result);
 }
